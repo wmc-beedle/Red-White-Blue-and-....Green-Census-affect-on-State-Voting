74a2ca7e44c26a8f474d212a55a333e20b9b06a0
wmc-beedle <williamcbeedle@gmail.com>
2020-10-29 14:36:54 -0700

    Made California graph into scatterplot of every state

--- census_con.py
+++ census_con.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
+import matplotlib.patches as mpatches
+
 
 # Import data
 income = pd.read_excel('Census_median_income_1984_2019.ods', engine='odf',header=[59])
@@ -49,40 +50,45 @@ income_politics_table = politics.merge(median, on=['year','state'], how='outer')
 income_politics_table = income_politics_table.merge(error, on=['year','state'], how='outer')
 
 # state_list = president['state']
-
-#income_politics_table.to_excel('Income_vs_Politics.xlsx')
+# income_politics_table.to_excel('Income_vs_Politics.xlsx')
 # income_politics_table = income_politics_table.reset_index().set_index(['year','state'])
-
+# cali_mi_win_party = median_income_state[median_income_state.index >= 1984]
 
 ipt = income_politics_table
-
-ipt_c = ipt[ipt['state']=='California']
-ipt_c =ipt_c[ipt_c['year'] >= 1984]
-mi = ipt_c.groupby(['year']).mean()['Median Income']
-
-
-max_v = ipt_c.groupby('year')['candidatevotes'].transform(func=max)
-ipt_c.groupby(['year'])
-
-
-cali_win_party = ipt_c[ipt_c['candidatevotes']==max_v]
-mi_c = cali_win_party.groupby('year').mean()['Median Income']
-cali_mi_win_party = mi_c[mi_c.index >= 1984]
-cali_democrat = cali_win_party[cali_win_party['party'] == 'democrat']
-cdi = cali_democrat['Median Income']
-cali_republican = cali_win_party[cali_win_party['party'] == 'republican']
-cri = cali_republican['Median Income']
-
 fig, ax = plt.subplots()
-ax.set_title("Median Income over Time")
+
+for state in pd.unique(ipt['state']):
+    #Median Income by State between 1984 and 2016
+    ipt_state = ipt[ipt['state']==state]
+    ipt_state = ipt_state[ipt_state['year'] >= 1984]
+    ipt_state.groupby(['year']).mean()['Median Income']
+
+    #Winner Table
+    max_v = ipt_state.groupby('year')['candidatevotes'].transform(func=max)
+    ipt_state.groupby(['year'])
+
+    #Winner of each State
+    state_winning_party = ipt_state[ipt_state['candidatevotes']==max_v]
+    median_income_state = state_winning_party.groupby('year').mean()['Median Income']
+
+    #Political Party Plotting
+    democrat = state_winning_party[state_winning_party['party'] == 'democrat']
+    democrat_median_income = democrat['Median Income']
+    republican = state_winning_party[state_winning_party['party'] == 'republican']
+    republican_median_income = republican['Median Income']
+
+    ax.scatter(democrat.iloc[:,0],democrat_median_income,c='blue')
+    ax.scatter(republican.iloc[:,0],republican_median_income,c='red')
+
+
+
+red_patch = mpatches.Patch(color='red', label='Republican Won State')
+blue_patch = mpatches.Patch(color='blue', label='Democrat Won State')
+
+plt.legend(handles=[red_patch, blue_patch], loc='best')
+
+ax.set_title("States' Political Preference over Time compared to Median Household Income")
 ax.set_xlabel("Year")
 ax.set_ylabel("Median Income")
-
-
-ax.scatter(cali_democrat.iloc[:,0],cdi,c='blue',label='democrat')
-ax.scatter(cali_republican.iloc[:,0],cri,c='red',label='republican')
-
-
-ax.legend(loc='best')
 plt.show()
 
